8ea94022107730f07a14944b4d5b423bdbca361b
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-12-22 18:02:58 +0900

    apollos server を実装

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,20 +1,50 @@
-import * as express from "express";
-import { Request, Response } from "express";
-import { music } from "src/music";
+import { ApolloServer, gql } from "apollo-server";
 
-const app = express();
-const { PORT = 3000 } = process.env;
+const authors = [{ id: 1, name: "Nakanishi" }, { id: 2, name: "Sasaki" }];
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({
-    message: "hello world!" + music + "is music abc"
-  });
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+const typeDefs = gql`
+  type Author {
+    name: String
+    id: Int!
+    blar: Boolean!
+  }
+
+  type Query {
+    getAuthor(id: Int!): [Author]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getAuthor: () => authors
+  },
+  Author: {
+    name: (_, __, ___) => "my name",
+    id: () => 2,
+    blar: (author, _, __) => {
+      return true;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log("server started at http://localhost:" + PORT);
-  });
-}
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
 
-export default app;
+export default server;
